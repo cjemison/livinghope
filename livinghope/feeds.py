643e6c5c0e29604714fec3d9bb295175c101bc80
takeahsiaor <rhsiao2@gmail.com>
2015-01-22 23:18:07 -0600

    More changes to make it itunes compatible

--- livinghope/feeds.py
+++ livinghope/feeds.py
@@ -2,17 +2,49 @@ from django.contrib.syndication.views import Feed
 from django.core.urlresolvers import reverse
 from livinghope.models import Sermon
 import views
-from mutagen.mp3 import MP3 # this is to find duration of recordings
+from mutagen.mp3 import MP3
 from django.core.urlresolvers import reverse
+from django.utils.feedgenerator import Rss201rev2Feed
+
+# For more details on the Podcast "Spec" see: https://www.apple.com/itunes/podcasts/specs.html
+
+class iTunesPodcastsFeedGenerator(Rss201rev2Feed):
+    def rss_attributes(self):
+        return {u"version": self._version,
+                u"xmlns:atom": u"http://www.w3.org/2005/Atom",
+                u'xmlns:itunes': u'http://www.itunes.com/dtds/podcast-1.0.dtd'}
+
+    def add_root_elements(self, handler):
+        super(iTunesPodcastsFeedGenerator, self).add_root_elements(handler)
+        handler.addQuickElement(u'itunes:subtitle', self.feed['subtitle'])
+        handler.addQuickElement(u'itunes:author', self.feed['author_name'])
+        handler.addQuickElement(u'itunes:summary', self.feed['description'])
+        handler.addQuickElement(u'itunes:explicit',
+                                self.feed['iTunes_explicit'])
+        handler.startElement(u"itunes:owner", {})
+        handler.addQuickElement(u'itunes:name', self.feed['iTunes_name'])
+        handler.addQuickElement(u'itunes:email', self.feed['iTunes_email'])
+        handler.endElement(u"itunes:owner")
+        handler.addQuickElement(u'itunes:image', self.feed['iTunes_image_url'])
+
+    def add_item_elements(self, handler, item):
+        super(iTunesPodcastsFeedGenerator, self).add_item_elements(
+            handler, item)
+        handler.addQuickElement(u'itunes:duration', item['duration'])
+        handler.addQuickElement(u'itunes:explicit', item['explicit'])
+        handler.addQuickElement(u'itunes:author', item['author'])
+
 
 class LatestSermonsFeed(Feed):
+    feed_type = iTunesPodcastsFeedGenerator
     title = "Living Hope Sermons"
     link = "/sitenews/"
     description = "The latest two sermons preached at Living Hope."
-
-    iTunes_name = u'Living Hope Sermons'
-    iTunes_email = u'info@onelivinghope.com'
-    iTunes_image_url = u''
+    subtitle = description
+    summary = description
+    iTunes_name = u'Free Law Project'
+    iTunes_email = u'feeds@courtlistener.com'
+    iTunes_image_url = u'https://www.courtlistener.com/static/png/producer.png'
     iTunes_explicit = u'no'
 
     def feed_extra_kwargs(self, obj):
@@ -22,14 +54,11 @@ class LatestSermonsFeed(Feed):
                 'iTunes_explicit': self.iTunes_explicit}
 
     def item_extra_kwargs(self, item):
-        audio = MP3(item.path)
+        audio = MP3(item.recording.path)
         duration = audio.info.length
-        return {'author': item.author,
-                'duration': duration,
+        return {'author': item.author.full_name(),
+                'duration': str(duration),
                 'explicit': u'no'}
-
-    def author_name(self, item):
-        return item.author
 
     def items(self):
         return Sermon.objects.all().exclude(recording='').order_by('-sermon_date')[:2]
@@ -48,10 +77,10 @@ class LatestSermonsFeed(Feed):
         return item.recording.url
 
     def item_enclosure_length(self,item):
-        return item.recording.size
-
-    def item_pubdate(self, item):
-        return item.sermon_date
+        try:
+            return item.recording.size
+        except:
+            return 5000
 
     item_enclosure_mime_type = 'audio/mpeg'
     # def item_enclosure_mime_type(self, item):
